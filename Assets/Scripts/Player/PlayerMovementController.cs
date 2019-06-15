143b7c032dc1444c09561fc98f08c2c860586502
Maciek Sowiar <dark.seer333@gmail.com>
2019-06-15 17:05:23 +0200

    Added movement particleSystem and dust effect

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour {
 
     public float playerSpeed = 10f;
+    public ParticleSystem particleSystem;
+    [SerializeField] float startTimeBetweenTrail = 0.4f;
     Animator animator;
-
+    float timeBetweenTrail;
     void Start() {
         animator = GetComponent<Animator>();
+        timeBetweenTrail = startTimeBetweenTrail;
     }
 
     void Update() {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        SetMovementAnimationParams(GetPlayerInputVector());
+        Movement(GetPlayerInputVector());
+        if(GetPlayerInputVector().magnitude > 0) {
+            if(timeBetweenTrail <= 0) {
+                particleSystem.Play();
+                timeBetweenTrail = startTimeBetweenTrail;
+            }
+            else {
+                timeBetweenTrail -= Time.deltaTime;
+            }
+        }
+        if(GetPlayerInputVector() != Vector3.zero) {
+            Vector3 previousNonZeroInput = GetPlayerInputVector();
+        }
+    }
+
+    void SetMovementAnimationParams(Vector3 movement) {
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Magnitude", movement.magnitude);
-        transform.position = transform.position + movement * Time.deltaTime * playerSpeed;
+    }
+
+    Vector3 GetPlayerInputVector() {
+        return new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+    }
+
+    void Movement(Vector3 input) {
+        transform.position = transform.position + input * Time.deltaTime * playerSpeed;
     }
 
 }
